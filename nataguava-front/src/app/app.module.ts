644381f5d6f429a3cc54206e39ccee5442e6f5dd
Denis <dineresc@gmail.com>
2019-02-22 14:15:00 -0300

    merge conflit

--- nataguava-front/src/app/app.module.ts
+++ nataguava-front/src/app/app.module.ts
@@ -6,12 +6,9 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { VacancyFormComponent } from './pages/vacancy/vacancy-form/vacancy-form.component';
 import { routes } from './app.routes';
-<<<<<<< HEAD
-=======
-import { SignupCompanyComponent } from './pages/signup/signup-company/signup-company.component';
 import { SignupCandidateComponent } from './pages/signup/signup-candidate/signup-candidate.component';
->>>>>>> 7538521d4677b5641c8e6f1f332abff8ece27c40
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignupCompanyComponent } from './pages/signup/signup-company/signup-company.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +16,8 @@ import { SignInComponent } from './pages/sign-in/sign-in.component';
     HeaderComponent, 
     MenuComponent,
     VacancyFormComponent,
-<<<<<<< HEAD
-    SignInComponent,
-=======
     SignupCompanyComponent,
     SignupCandidateComponent,
->>>>>>> 7538521d4677b5641c8e6f1f332abff8ece27c40
     SignInComponent
   ],
   exports:[
--- nataguava-front/src/app/app.routes.ts
+++ nataguava-front/src/app/app.routes.ts
@@ -1,14 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
-import { v } from '@angular/core/src/render3';
 import { VacancyFormComponent } from './pages/vacancy/vacancy-form/vacancy-form.component';
 import { ModuleWithProviders } from '@angular/core';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
-<<<<<<< HEAD
-
-export const ROUTES: Routes = [
-    {path: 'vacancy-form', component: VacancyFormComponent},
-    {path: 'login', component: SignInComponent}
-=======
 import { SignupCompanyComponent } from './pages/signup/signup-company/signup-company.component';
 import { SignupCandidateComponent } from './pages/signup/signup-candidate/signup-candidate.component';
 
@@ -17,7 +10,6 @@ export const ROUTES: Routes = [
     {path: 'login', component: SignInComponent},
     {path: 'signup-company', component: SignupCompanyComponent},
     {path: 'signup-candidate', component: SignupCandidateComponent},
->>>>>>> 7538521d4677b5641c8e6f1f332abff8ece27c40
 ]
 
 export const routes:  ModuleWithProviders = RouterModule.forRoot(ROUTES);
