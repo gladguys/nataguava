74fe1fc36db0b9ce1000658d45479f7a44ee62e5
Denis <dineresc@gmail.com>
2019-02-26 17:52:06 -0300

    solve conflit

--- nataguava-front/src/app/app.module.ts
+++ nataguava-front/src/app/app.module.ts
@@ -4,30 +4,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
-import { VacancyFormComponent } from './pages/vacancy/vacancy-form/vacancy-form.component';
 import { routes } from './app.routes';
 import { SignupCandidateComponent } from './pages/signup/signup-candidate/signup-candidate.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignupCompanyComponent } from './pages/signup/signup-company/signup-company.component';
-import { CompanyComponent } from './pages/company/company.component';
-import { CompanyFormComponent } from './pages/company/company-form/company-form.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
     HeaderComponent, 
     MenuComponent,
-    VacancyFormComponent,
     SignupCompanyComponent,
     SignupCandidateComponent,
-    SignInComponent,
-    CompanyComponent,
-    CompanyFormComponent
+    SignInComponent
   ],
   exports:[
-    HeaderComponent,
-    VacancyFormComponent,
-  
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
