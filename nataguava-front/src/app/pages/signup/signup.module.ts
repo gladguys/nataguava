180ecccba6065b7ec38d3fa99f936f269648284b
Denis Caminha <dineresc@gmail.com>
2019-03-02 15:29:41 -0300

    module form signup

---
+++ nataguava-front/src/app/pages/signup/signup.module.ts
@@ -0,0 +1,17 @@
+import { SignupCandidateComponent } from './signup-candidate/signup-candidate.component';
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+    declarations: [SignupCandidateComponent],
+    imports: [
+        HttpClientModule,
+        ReactiveFormsModule,
+        CommonModule
+    ]
+})
+export class SignInModule {
+
+}
