0641918ec4b1dd0a920687a9e66cb358dfa1ebc3
Denis Caminha <dineresc@gmail.com>
2019-03-02 14:48:34 -0300

    creates a user that will be common to candidate and company

---
+++ nataguava-api/src/main/java/br/com/daboiud/nataguava/models/User.java
@@ -0,0 +1,52 @@
+package br.com.daboiud.nataguava.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String email;
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    private ProfileEnum profileEnum;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ProfileEnum getProfileEnum() {
+        return profileEnum;
+    }
+
+    public void setProfileEnum(ProfileEnum profileEnum) {
+        this.profileEnum = profileEnum;
+    }
+}
