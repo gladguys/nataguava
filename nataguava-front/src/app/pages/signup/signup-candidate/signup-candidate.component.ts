4bdb88c057704e5982e731bcdd37d028d6d1346f
Denis Caminha <dineresc@gmail.com>
2019-03-02 15:28:43 -0300

    adapting form to reactive form angular

--- nataguava-front/src/app/pages/signup/signup-candidate/signup-candidate.component.ts
+++ nataguava-front/src/app/pages/signup/signup-candidate/signup-candidate.component.ts
@@ -1,15 +1,26 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-signup-candidate',
   templateUrl: './signup-candidate.component.html',
   styleUrls: ['./signup-candidate.component.css']
 })
 export class SignupCandidateComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+  
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.fb.group({
+      'name': ['', Validators.required],
+      'email': ['', Validators.compose([Validators.required, Validators.email])],
+      'password': ['', Validators.required],
+      'urlRepo': ['',]
+    });
   }
 
+  signup() {
+  
+  }
 }
