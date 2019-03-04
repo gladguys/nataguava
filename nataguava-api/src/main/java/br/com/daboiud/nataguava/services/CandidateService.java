9d0305dfe84d8f2e6f56d32bceba15a57009b2d5
Denis Caminha <dineresc@gmail.com>
2019-03-04 02:39:58 -0300

    creating method to signup service

--- nataguava-api/src/main/java/br/com/daboiud/nataguava/services/CandidateService.java
+++ nataguava-api/src/main/java/br/com/daboiud/nataguava/services/CandidateService.java
@@ -1,9 +1,11 @@
 package br.com.daboiud.nataguava.services;
 
 import br.com.daboiud.nataguava.models.Candidate;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public interface CandidateService {
 
     Candidate createOrUpdate(Candidate candidate);
--- nataguava-api/src/main/java/br/com/daboiud/nataguava/services/impl/CandidateServiceImpl.java
+++ nataguava-api/src/main/java/br/com/daboiud/nataguava/services/impl/CandidateServiceImpl.java
@@ -1,18 +1,26 @@
 package br.com.daboiud.nataguava.services.impl;
 
 import br.com.daboiud.nataguava.models.Candidate;
+import br.com.daboiud.nataguava.repositories.CandidateRepository;
 import br.com.daboiud.nataguava.services.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CandidateServiceImpl implements CandidateService {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @Override
     public Candidate createOrUpdate(Candidate candidate) {
-        return null;
+        return this.candidateRepository.save(candidate);
     }
 
     @Override
     public List<Candidate> findAll() {
-        return null;
+        return this.candidateRepository.findAll();
     }
 }
