6611a691879d83bfcb6a149cb206fbc339ecf0d3
wesleyhenrique7 <wsantos.inbox@gmail.com>
2019-04-28 18:23:10 -0300

    Criando componente

---
+++ nataguava-front/src/app/pages/admin/question-form/question-form.component.ts
@@ -0,0 +1,73 @@
+import { OnInit, Component } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { ItemQuestion } from "src/app/models/item-question.model";
+import { Question } from "src/app/models/question.model";
+import { QuestionService } from "src/app/services/question.service";
+
+@Component({
+  templateUrl: './question-form.component.html',
+  styleUrls: ['./question-form.component.css']
+})
+export class QuestionFormComponent implements OnInit {
+
+    questionForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder,
+                private questionService: QuestionService) { }
+
+    ngOnInit() {
+      this.questionForm = this.formBuilder.group({
+        'title': ['', Validators.required],
+        'item1': ['', Validators.required],
+        'item2': ['', Validators.required],
+        'item3': ['', Validators.required],
+        'item4': ['', Validators.required],
+        'rightAnswer' : ['', Validators.required]
+      });
+    }
+
+    submit() {
+        let question = new Question();
+        question.title = this.questionForm.controls['title'].value;
+
+        let item1  = new ItemQuestion();
+        item1.title = this.questionForm.controls['item1'].value;
+
+        let item2  = new ItemQuestion();
+        item2.title = this.questionForm.controls['item2'].value;
+
+        let item3  = new ItemQuestion();
+        item3.title = this.questionForm.controls['item3'].value;
+
+        let item4  = new ItemQuestion();
+        item4.title = this.questionForm.controls['item4'].value;
+
+        let itemCerto : string = this.questionForm.controls['rightAnswer'].value;
+
+        switch(itemCerto) {
+          case 'a':
+            item1.correct = true;
+            break;
+          case 'b':
+            item2.correct = true;
+            break;
+          case 'c':
+            item3.correct = true;
+            break;
+          case 'd':
+            item4.correct = true;
+            break;
+        }
+
+        let itensQuestion = Array<ItemQuestion>();
+
+        itensQuestion.push(item1, item2, item3, item4); 
+
+        question.itens = itensQuestion;
+
+        console.log(question);
+        this.questionService.save(question).subscribe((questionCreated: Question) => {
+          alert("Questão criada com sucesso!");
+        });
+    }
+  }
