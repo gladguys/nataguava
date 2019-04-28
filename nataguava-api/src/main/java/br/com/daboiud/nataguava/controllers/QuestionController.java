321404de538c20b706e4406f3561bfae2c0f9881
wesleyhenrique7 <wsantos.inbox@gmail.com>
2019-04-28 20:10:18 -0300

    Criando question controller

---
+++ nataguava-api/src/main/java/br/com/daboiud/nataguava/controllers/QuestionController.java
@@ -0,0 +1,29 @@
+package br.com.daboiud.nataguava.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.daboiud.nataguava.models.Question;
+import br.com.daboiud.nataguava.services.QuestionService;
+
+@RestController
+@RequestMapping(value = "/api/question")
+public class QuestionController {
+
+	private QuestionService questionService;
+
+	@PostMapping
+    public ResponseEntity<Question> save(@RequestBody Question question) {
+        Question savedQuestion;
+        try {
+        	savedQuestion = this.questionService.save(question);
+            return ResponseEntity.ok(savedQuestion);
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+}
