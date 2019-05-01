61a170130e26733a764c994ed71b56e385abc6fb
wesleyhenrique7 <wsantos.inbox@gmail.com>
2019-05-01 17:31:47 -0300

    Adicionando mais valores ao enum

--- nataguava-api/src/main/java/br/com/daboiud/nataguava/models/ContentTag.java
+++ nataguava-api/src/main/java/br/com/daboiud/nataguava/models/ContentTag.java
@@ -3,5 +3,19 @@ package br.com.daboiud.nataguava.models;
 public enum ContentTag {
 
     JAVA,
-    JAVASCRIPT
+    JAVASCRIPT,
+    PHP, 
+    SQL, 
+    ANDROID, 
+    PYTHON, 
+    RUBY, 
+    RAILS, 
+    IONIC,
+    C_SHARP,
+    C_PLUS_PLUS, 
+    SPRING, 
+    JSF, 
+    ESTRUTURA_DE_DADOS,
+    REACT,
+    CLOJURE
 }
