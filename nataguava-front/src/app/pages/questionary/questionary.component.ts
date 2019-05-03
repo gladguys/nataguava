8ae1a99f6c192aa73fc85dbf29a28bfc6e211f66
dicaminha <dineresc@gmail.com>
2019-05-02 21:33:32 -0300

    mais estilos

--- nataguava-front/src/app/pages/questionary/questionary.component.ts
+++ nataguava-front/src/app/pages/questionary/questionary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-questionary',
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionaryComponent implements OnInit {
 
+  @ViewChild("q1") q1: ElementRef;
+  @ViewChild("q2") q2: ElementRef;
+  @ViewChild("q3") q3: ElementRef;
+  @ViewChild("q4") q4: ElementRef;
+
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  q1Clicked() {
+    this.q1.nativeElement.style.opacity = 1.0;
+    
+    this.q2.nativeElement.style.opacity = 0.3;
+    this.q3.nativeElement.style.opacity = 0.3;
+    this.q4.nativeElement.style.opacity = 0.3;
+  }
+
+  q2Clicked() {
+    this.q2.nativeElement.style.opacity = 1.0;
+
+    this.q1.nativeElement.style.opacity = 0.3;
+    this.q3.nativeElement.style.opacity = 0.3;
+    this.q4.nativeElement.style.opacity = 0.3;
+  }
+  q3Clicked() {
+    this.q3.nativeElement.style.opacity = 1.0;
+
+    this.q1.nativeElement.style.opacity = 0.3;
+    this.q2.nativeElement.style.opacity = 0.3;
+    this.q4.nativeElement.style.opacity = 0.3;
+  }
+
+  q4Clicked() {
+    this.q4.nativeElement.style.opacity = 1.0;
+
+    this.q1.nativeElement.style.opacity = 0.3;
+    this.q2.nativeElement.style.opacity = 0.3;
+    this.q3.nativeElement.style.opacity = 0.3;
   }
 
 }
