d74f10015589e5e48ceca57cff8bfa9a66463953
Wesley <wsantos.inbox@gmail.com>
2019-05-19 18:10:19 -0300

    Fazendo a lógica que realiza a troca dinâmica do title do browser

--- nataguava-front/src/app/app.component.ts
+++ nataguava-front/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from './services/shared.service';
+import { ActivatedRoute, Router, NavigationEnd, RouterEvent } from '@angular/router';
+import { filter, map, switchMap } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +11,27 @@ import { SharedService } from './services/shared.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'nataguava-front';
-
   userIsLoggedIn$ = new Observable<boolean>(null);
 
-  constructor(private sharedService: SharedService,){}
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private titleService: Title,
+              private sharedService: SharedService){}
 
-
-  ngOnInit(){
+  ngOnInit() {
+    this.setTitle();
     this.userIsLoggedIn$ = this.sharedService.userIsLoggedIn$;
   }
 
+  setTitle() {
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .pipe(map(() => this.activatedRoute))
+    .pipe(map(route => {
+      while (route.firstChild) route = route.firstChild;
+      return route;
+    }))
+    .pipe(switchMap(route => route.data))
+    .subscribe(event => this.titleService.setTitle(event.title));
+  }
 }
