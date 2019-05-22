c156e17442a3ac26a8b7ad6630766c8995a0b4a9
Denis Neres Caminha <dineresc@gmail.com>
2019-05-22 13:54:34 -0300

    criando arcabouco para componente job ranking

---
+++ nataguava-front/src/app/pages/job/job-ranking/job-ranking.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'guava-job-ranking',
+    templateUrl: './job-ranking.component.html',
+    styleUrls: ['job-ranking.component.css']
+})
+export class JobRankingComponent {
+
+    constructor() {}
+}
